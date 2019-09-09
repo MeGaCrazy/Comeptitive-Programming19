b9581f3c87e9f8958ab5c294880f91c2a14e477f
MeGaCrazy <mikaelmounir@gmail.com>
2019-09-09 04:09:39 +0200

    a'

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**
+*   @Author: MeGaaaaaaaaaa
+*   @InitTime: Mon Sep 09 04:07:51 2019
+*   @Idea: 
+*
+*
+*
+*
+* Happy Solving :)
+**/
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("in","r",stdin)
+    freopen("out","w",stdout)
+#endif
+
+
+
+
+
+
+
+}
